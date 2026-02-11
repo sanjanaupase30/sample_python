51a798f9306e3cd84f0d01455a7682f6fd425068
sanjanaupase30 <sanjanaupase886@gmail.com>
2026-02-11 13:07:02 +0530

    Add files via upload

---
+++ elifsamp.py
@@ -0,0 +1,7 @@
+marks=int(input("Enter your marks"))
+if(marks<=40):
+    print("you are failed")
+elif(marks>=70):
+    print("you are passed with first class")
+else:
+    print("you are passed in first class distinction")
